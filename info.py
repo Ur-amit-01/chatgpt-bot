853f94afe7414cdd2665d69c08d300d66389e35c
Ur-amit-01 <heroku467@gmail.com>
2025-01-09 16:48:06 +0530

    Update info.py

--- info.py
+++ info.py
@@ -4,15 +4,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = environ.get("API_ID" , "2099007")
-API_HASH = environ.get("API_HASH" , "b1b3d7b3b1b3d7b3b1b3d7b3b1b3d7b3")
-BOT_TOKEN = environ.get("BOT_TOKEN" , "2099007:AAH1b3d7b3b1b3d7b3b1b3d7b3b1b3d7b3")
-ADMIN = int(environ.get("ADMIN" , "1847899007"))
-CHAT_GROUP = int(environ.get("CHAT_GROUP", "-100"))
-LOG_CHANNEL = environ.get("LOG_CHANNEL", "-100")
+API_ID = environ.get("API_ID" , "22012880")
+API_HASH = environ.get("API_HASH" , "5b0e07f5a96d48b704eb9850d274fe1d)
+BOT_TOKEN = environ.get("BOT_TOKEN" , "")
+ADMIN = int(environ.get("ADMIN" , "7728066109"))
+CHAT_GROUP = int(environ.get("CHAT_GROUP", "-1004758607891"))
+LOG_CHANNEL = environ.get("LOG_CHANNEL", "-1002355632078")
 MONGO_URL = environ.get("MONGO_URL" , "mongodb://localhost:27017")
 AUTH_CHANNEL = int(
-    environ.get("AUTH_CHANNEL", "-1001734958816")
+    environ.get("AUTH_CHANNEL", "-1002355632078)
 )
 FSUB = environ.get("FSUB", True)
 STICKERS_IDS = (
@@ -20,6 +20,6 @@ STICKERS_IDS = (
 ).split()
 COOL_TIMER = 20  # keep this atleast 20
 ONLY_SCAN_IN_GRP = environ.get(
-    "ONLY_SCAN_IN_GRP", True
+    "ONLY_SCAN_IN_GRP", False
 )  # If IMG_SCAN_IN_GRP is set to True, image scanning is restricted to your support group only. If it's False, the image scanning feature can be used anywhere.
 REACTIONS = ["❤️‍🔥", "⚡", "🔥"]
