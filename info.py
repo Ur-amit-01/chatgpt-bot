8605f93a4879d2cbcfbf2a9eb85046d71518ad46
Ur-amit-01 <heroku467@gmail.com>
2025-01-09 17:06:25 +0530

    Update info.py

--- info.py
+++ info.py
@@ -4,14 +4,14 @@ from os import environ
 load_dotenv()
 
 API_ID = environ.get("API_ID" , "22012880")
-API_HASH = environ.get("API_HASH" , "5b0e07f5a96d48b704eb9850d274fe1d)
+API_HASH = environ.get("API_HASH" , "5b0e07f5a96d48b704eb9850d274fe1d")
 BOT_TOKEN = environ.get("BOT_TOKEN" , "")
 ADMIN = int(environ.get("ADMIN" , "7728066109"))
 CHAT_GROUP = int(environ.get("CHAT_GROUP", "-1004758607891"))
 LOG_CHANNEL = environ.get("LOG_CHANNEL", "-1002355632078")
 MONGO_URL = environ.get("MONGO_URL" , "mongodb://localhost:27017")
 AUTH_CHANNEL = int(
-    environ.get("AUTH_CHANNEL", "-1002355632078)
+    environ.get("AUTH_CHANNEL", "-1002355632078")
 )
 FSUB = environ.get("FSUB", True)
 STICKERS_IDS = (
